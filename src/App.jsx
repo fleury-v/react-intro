255ccf1e29dbaf3bd5f8a919968ce56d07ae9ba2
Fleury VUADIAMBO <matanu.fv@gmail.com>
2025-09-19 17:38:08 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Greetings } from "./Greetings"
+
+export function App() {
+  return (
+  <div>
+    <Greetings />
+    <input type="text" /> 
+    <button> Submit </button>
+  </div>
+  )
+}
